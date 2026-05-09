2b817c69e4f61d6a4208840a57fe61de3829b4b5
RK-YADDAV <riteshkumar20092005@gmail.com>
2026-05-09 19:32:32 +0530

    Restrict CORS to specific origins for enhanced security

--- backend/main.py
+++ backend/main.py
@@ -35,7 +35,7 @@ app = FastAPI(title="Gemini Chatbot API")
 # Allow requests from any origin (CORS)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173","https://infollion-ai.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
